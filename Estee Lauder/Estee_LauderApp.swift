a38894b5d38ea8eb562dd4a2c80747f9e8a0df48
Leyna <leynaphan16@gmail.com>
2023-08-03 00:37:45 -0500

    Initial Commit

---
+++ Estee Lauder/Estee_LauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_LauderApp.swift
+//  Estee Lauder
+//
+//  Created by Leyna Phan on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_LauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
